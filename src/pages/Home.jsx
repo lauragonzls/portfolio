34a2c7c046d6337a8b3b8ed0e4fc71f1879fa3eb
Laura Gonzalez <lauraglslv@gmail.com>
2025-07-25 23:59:38 -0500

    chore: improvements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,19 +6,26 @@ function Home() {
         <div className="text-center p-5">
             <nav>
                 <div>
-                    <img src="hamster.png" alt="Logo" className="logo" />
+                    <img src="Logo.png" alt="Logo" className="logo" />
                 </div>
                 <ul>
                     <li><a href="/">Home</a></li>
                     <li><a href="/about">About</a></li>
                     <li><a href="/skills">Skills</a></li>
+                    <li><a href="/">Contact</a></li>
                 </ul>
             </nav>
             <section className="home-section">
-                <h1>Hi there!</h1>
-                <h4 className='text-muted'>I'm Laura</h4>
-                <h3 >System Engineer</h3>
-                <button className='cv-button'>CV</button>
+                <div>
+                    <div className="greeting">👋 Hey there!</div>
+                    <h2>Laura Gonzalez </h2>
+                    <h4>Fullstack developer</h4>
+                    <button className='cv-button'>CV</button>
+                </div>
+                <div>
+                    <img src="Profile.png" alt="Laura" className="profile-image" />
+                </div>
+
             </section>
 
         </div>
